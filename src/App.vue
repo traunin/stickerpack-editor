698e1ac2b5eab6451ffb3b707c2ff3a0042205e8
Traunin <traunin5@gmail.com>
2024-11-06 03:35:04 +0300

    feat: strerch app to fit full page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-    <navbar></navbar>
-    <router-view></router-view>
+    <div class="container">
+        <navbar></navbar>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue';
 </script>
+
+<style scoped>
+.container {
+    position: fixed;
+    background: #000;
+    color: #fff;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
