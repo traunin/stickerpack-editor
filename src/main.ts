8e521af9186fb75b07ee1f21caf508a4d50348d3
Traunin <traunin5@gmail.com>
2024-11-06 04:06:37 +0300

    feat: set default font as Roboto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import router from "@/routes.ts"
-import '@/assets/reset.css'
+
+import '@/assets/styles/reset.css'
+import '@/assets/styles/style.css'
+
 import App from '@/App.vue'
 
 const app = createApp(App)
