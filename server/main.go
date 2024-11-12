e45124c9eefb2ef06167feea764e69b43ceaa5de
Traunin <traunin5@gmail.com>
2024-11-12 08:54:17 +0300

    feat: bot test setup

--- server/main.go
+++ server/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	telebot "gopkg.in/telebot.v3"
+)
+func main() {
+    bot, err := telebot.NewBot(telebot.Settings {
+        Token: BotToken,
+    })
+
+    if (err != nil) {
+        print(err)
+    }
+
+    // bot.createNewStickerSet
+    bot.Handle("/start", func(c telebot.Context) error {
+        fmt.Printf("Message from %d\n", c.Chat().ID)
+		return c.Send(fmt.Sprintf("%d", c.Chat().ID))
+	})
+
+	bot.Start()
 }
