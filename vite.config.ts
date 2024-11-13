b5e9d52fee6dee8a83f65af8a64e674b601cd129
Traunin <traunin5@gmail.com>
2024-11-13 21:24:02 +0300

    feat: move over to http

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    basicSsl()
   ],
 
   server: {
-    port: 443
+    port: 80
   },
   
   resolve: {
