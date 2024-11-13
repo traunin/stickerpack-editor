e41698205024548c0b8a09ef93cd16d80abc6a5e
Traunin <traunin5@gmail.com>
2024-11-14 01:20:10 +0300

    feat: setup stickerpack rendering

--- src/views/MyStickerpacks.vue
+++ src/views/MyStickerpacks.vue
@@ -1,3 +1,44 @@
 <template>
-    My stickerpacks
+    <div class="stickerpacks">
+        <stickerpack-preview
+            v-for="stickerpack in stickerpacks"
+            :name="stickerpack['name']"
+        ></stickerpack-preview>
+    </div>
 </template>
+
+<script setup lang = "ts">
+import StickerpackPreview from '@/components/StickerpackPreview.vue';
+import { ref } from 'vue'
+
+type Stickerpack = {
+    name: string
+}
+
+const loading = ref(false)
+const stickerpacks = ref<null | Stickerpack[]>(null)
+
+fetchStickerpacks();
+
+async function fetchStickerpacks() {
+  loading.value = true
+  
+  try {
+    stickerpacks.value = 
+        await fetch("http://7tvstickerpacks.website:8080/api/stickerpacks")  
+            .then(res => res.json())
+  } catch (err) {
+    console.log(err)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<style scoped>
+.stickerpacks {
+    margin: 20px;
+    display: flex;
+    gap: 20px;
+}
+</style>
