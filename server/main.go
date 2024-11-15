b15f656fb28e2030eb533b4d1d6c3c0a7fa76957
Traunin <traunin5@gmail.com>
2024-11-15 23:48:05 +0300

    feat: stickerpack creation setup

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"gopkg.in/telebot.v3"
 )
@@ -39,14 +40,67 @@ func LaunchBot() {
 	bot.Start()
 }
 
-func main() {
-	http.HandleFunc("/api/stickerpacks", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+type TelegramRecipient struct {
+	chatID string
+}
 
-		json.NewEncoder(w).Encode(StickerpackMocks)
+func (r TelegramRecipient) Recipient() string {
+	return r.chatID
+}
+
+func getUserById(id int64) {
+	bot, err := telebot.NewBot(telebot.Settings{
+		Token: BotToken,
 	})
+
+	if err != nil {
+		print(err)
+	}
+	recipient := &TelegramRecipient{strconv.Itoa(int(id))}
+	err = bot.CreateStickerSet(recipient, &telebot.StickerSet{
+		Type:     telebot.StickerRegular,
+		Format:   telebot.StickerStatic,
+		Name:     "skibidi_rizzling_rizzler124451",
+		Title:    "skibidi_rizzling_rizzler124451",
+		Animated: false,
+		Video:    false,
+		Stickers: []telebot.Sticker{},
+	})
+	if err != nil {
+		println("stickerpack error")
+		println(err)
+	}
+	bot.CreateStickerSet(recipient, &telebot.StickerSet{Name: "aboba2"})
+	fmt.Println(id)
+	chat, err := bot.ChatByID(id)
+
+	if err != nil {
+		println("No user with such id")
+	}
+
+	fmt.Println(chat.StickerSet)
+}
+
+func handleStickerpacks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	json.NewEncoder(w).Encode(StickerpackMocks)
+}
+
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	getUserById(int64(id))
+	// json.NewEncoder(w).Encode(StickerpackMocks)
+}
+
+func main() {
+	http.HandleFunc("/api/stickerpacks", handleStickerpacks)
+	http.HandleFunc("/api/user", handleUser)
 
 	http.ListenAndServe(":8080", nil)
 }
