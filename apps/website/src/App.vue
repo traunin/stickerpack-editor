99f818709c32617ac6d6b52b0347acf4a38860be
Traunin <traunin5@gmail.com>
2025-06-26 13:55:29 +0300

    feat: wrap to stretch to screen

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container dark">
     <NavbarHeader />
-    <RouterView />
+    <div class="content">
+      <RouterView />
+    </div>
   </div>
 </template>
 
@@ -12,12 +14,22 @@ import NavbarHeader from '@/components/navbar-header.vue'
 
 <style scoped>
 .container {
-    position: fixed;
-    background: var(--background);
-    color: var(--text);
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  background: var(--background);
+  color: var(--text);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.content {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
 }
 </style>
