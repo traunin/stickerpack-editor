a0baa625bd2257ca67828a78a7149edb17f9ef27
Traunin <traunin5@gmail.com>
2025-07-01 09:07:45 +0300

    feat: theme switch

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="container dark">
+  <div class="container">
     <NavbarHeader />
     <div class="content">
       <RouterView />
     </div>
+    <ThemeSwitch class="theme-switcher" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { useDark } from '@vueuse/core'
 import { RouterView } from 'vue-router'
 import NavbarHeader from '@/components/navbar-header.vue'
+import ThemeSwitch from './components/theme-switch.vue'
+
+useDark({
+  selector: 'body',
+  attribute: 'class',
+  valueDark: 'dark',
+  valueLight: 'light',
+})
 </script>
 
 <style scoped>
@@ -32,5 +42,11 @@ import NavbarHeader from '@/components/navbar-header.vue'
   justify-content: flex-start;
   align-items: flex-start;
   min-height: 0;
+}
+
+.theme-switcher {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
 }
 </style>
---
+++ apps/website/src/components/theme-switch.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button
+    type="button"
+    aria-label="Toggle dark mode"
+    @click="toggleDark()"
+  />
+</template>
+
+<style scoped>
+button {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  cursor: pointer;
+  margin: 0;
+  border:  2px solid var(--accent);
+  background: center url("@/assets/icons/sun.svg") white no-repeat;
+  background-size: 20px 20px;
+}
+
+.dark button {
+  background: center url("@/assets/icons/moon.svg") black no-repeat;
+  background-size: 20px 20px;
+}
+</style>
