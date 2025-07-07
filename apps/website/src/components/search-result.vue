363e1905b0b67dc4904ee96c3d9ca68048d95ac9
Traunin <traunin5@gmail.com>
2025-07-07 13:16:27 +0300

    feat: trim long emote names

--- apps/website/src/components/search-result.vue
+++ apps/website/src/components/search-result.vue
@@ -2,17 +2,26 @@
   <div class="emote">
     <img :src="emote.preview" :alt="emote.name">
     <div class="name">
-      {{ emote.name }}
+      {{ trimmedName }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { Emote } from '@/composables/use-emote-search'
 
-defineProps<{
+const props = defineProps<{
   emote: Emote
 }>()
+
+const MAX_NAME_LENGTH = 30
+const TRIMMED_LENGTH = 27
+const trimmedName = computed(() =>
+  props.emote.name.length < MAX_NAME_LENGTH ?
+    props.emote.name :
+    `${props.emote.name.substring(0, TRIMMED_LENGTH)}...`,
+)
 </script>
 
 <style scoped>
