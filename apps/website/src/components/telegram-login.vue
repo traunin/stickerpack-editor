06fc4da7b1a25914a4d95ea8c8f808eddf067b4e
Traunin <traunin5@gmail.com>
2025-07-12 00:03:50 +0300

    style: eslint

--- apps/website/src/components/telegram-login.vue
+++ apps/website/src/components/telegram-login.vue
@@ -1,51 +1,49 @@
 <template>
-    <div 
-        ref="telegramWidget" 
-        class="tg-auth-wrapper"
-    ></div>
+  <div
+    ref="telegramWidget"
+    class="tg-auth-wrapper"
+  />
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
-export type User = {
-    auth_date: number
-    first_name: string
-    hash: string 
-    id: number
-    photo_url: string
-    username: string
+export interface User {
+  auth_date: number
+  first_name: string
+  hash: string
+  id: number
+  photo_url: string
+  username: string
 }
 
 declare global {
-    interface Window {
-        onAuth: (user: User) =>  void
-    }
+  interface Window {
+    onAuth: (user: User) => void
+  }
 }
 
 const props = defineProps<{
-    onAuth: (user: User) => void;
-}>();
+  onAuth: (user: User) => void
+}>()
 
-
-const telegramWidget = ref<null | HTMLElement>(null);
+const telegramWidget = ref<null | HTMLElement>(null)
 
 onMounted(() => {
-    window.onAuth = props.onAuth;
+  window.onAuth = props.onAuth
 
-    const script = document.createElement('script');
-    script.setAttribute('data-telegram-login', 'seventv_stickerpack_bot');
-    script.setAttribute('data-size', 'large');
-    script.setAttribute('data-userpic', 'false');
-    script.setAttribute('data-radius', '10');
-    script.setAttribute('data-onauth', 'window.onAuth(user)');
-    script.setAttribute('data-request-access', 'write');
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.async = true;
+  const script = document.createElement('script')
+  script.setAttribute('data-telegram-login', 'seventv_stickerpack_bot')
+  script.setAttribute('data-size', 'large')
+  script.setAttribute('data-userpic', 'false')
+  script.setAttribute('data-radius', '10')
+  script.setAttribute('data-onauth', 'window.onAuth(user)')
+  script.setAttribute('data-request-access', 'write')
+  script.src = 'https://telegram.org/js/telegram-widget.js?22'
+  script.async = true
 
-
-    telegramWidget.value!.appendChild(script);
-});
+  telegramWidget.value!.appendChild(script)
+})
 </script>
 
 <style scoped>
