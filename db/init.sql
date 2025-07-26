282e5b105ad11a73f71952652e3d60a58087f968
Traunin <traunin5@gmail.com>
2025-07-27 00:03:32 +0300

    refactor: add boolean prefix

--- db/init.sql
+++ db/init.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS stickerpacks (
     id SERIAL PRIMARY KEY,
     user_id TEXT NOT NULL,
     title TEXT NOT NULL,
-    public BOOLEAN NOT NULL
+    is_public BOOLEAN NOT NULL
 )
