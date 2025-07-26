b7a39e34f94da92d7e6bbd89d86df16063a4baff
Traunin <traunin5@gmail.com>
2025-07-27 02:02:02 +0300

    fix: use BIGINT for user id

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS stickerpacks (
     id SERIAL PRIMARY KEY,
-    user_id TEXT NOT NULL,
+    user_id BIGINT NOT NULL,
     title TEXT NOT NULL,
     is_public BOOLEAN NOT NULL
 )
