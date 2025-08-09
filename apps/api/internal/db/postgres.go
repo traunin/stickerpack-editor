1a84340a20936f95c20a4fc289da9989ec79e583
Traunin <traunin5@gmail.com>
2025-08-09 16:09:33 +0300

    feat: public stickerpacks query

--- apps/api/internal/db/postgres.go
+++ apps/api/internal/db/postgres.go
@@ -10,15 +10,26 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const (
+	maxRetries              = 5
+	retryWait               = time.Second
+	insertStickerpackQuery  = `INSERT INTO stickerpacks (user_id, title, is_public) VALUES ($1, $2, $3)`
+	publicStickerpacksQuery = `SELECT id, title FROM stickerpacks WHERE is_public = true OFFSET $1 LIMIT $2`
+	countPacksQuery         = `SELECT COUNT(*) FROM stickerpacks WHERE is_public = true`
+)
+
 type Postgres struct {
 	db *sql.DB
 }
 
-const (
-	maxRetries = 5
-	retryWait  = time.Second
-	insertStickerpackQuery = `INSERT INTO stickerpacks (user_id, title, public) VALUES ($1, $2, $3)`
-)
+type PublicPack struct {
+	Id    int64  `json:"id"`
+	Title string `json:"title"`
+}
+
+func (s *PublicPack) ScanRow(rows *sql.Rows) error {
+	return rows.Scan(&s.Id, &s.Title)
+}
 
 func NewPostgres() *Postgres {
 	connStr := fmt.Sprintf(
@@ -47,7 +58,35 @@ func NewPostgres() *Postgres {
 	return nil
 }
 
-func (p Postgres) AddStickerpack(id string, title string, isPublic bool) (error) {
+func (p Postgres) AddStickerpack(id int64, title string, isPublic bool) error {
 	_, err := p.db.Exec(insertStickerpackQuery, id, title, isPublic)
 	return err
+}
+
+func (p Postgres) PublicStickerpacks(page, pageSize int) ([]PublicPack, error) {
+	offset := page * pageSize
+	rows, err := p.db.Query(publicStickerpacksQuery, offset, pageSize)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	stickerpacks := make([]PublicPack, 0, pageSize)
+
+	for rows.Next() {
+		var sp PublicPack
+		err := sp.ScanRow(rows)
+		if err != nil {
+			log.Printf("failed to scan stickerpack: %v", err)
+			continue
+		}
+		stickerpacks = append(stickerpacks, sp)
+	}
+
+	return stickerpacks, nil
+}
+
+func (p Postgres) PublicPacksCount() (int, error) {
+	var count int
+	err := p.db.QueryRow(countPacksQuery).Scan(&count)
+	return count, err
 }
