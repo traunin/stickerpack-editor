4792200cffe290cc2b7887032892b869405963c5
Traunin <traunin5@gmail.com>
2025-08-10 18:52:33 +0300

    feat: public packs endpoint

---
+++ apps/api/internal/api/get.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/Traunin/stickerpack-editor/apps/api/internal/config"
+	"github.com/Traunin/stickerpack-editor/apps/api/internal/db"
+)
+
+type GetPublicPacksResponse struct {
+	Packs []db.PublicPack `json:"packs"`
+	Total int             `json:"total"`
+}
+
+func getPacksHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	page, err := strconv.Atoi(query.Get("page"))
+	if err != nil {
+		http.Error(w, "page is not a number", http.StatusBadRequest)
+		return
+	}
+	pageSize, err := strconv.Atoi(query.Get("page_size"))
+	if err != nil {
+		http.Error(w, "page_size is not a number", http.StatusBadRequest)
+		return
+	}
+
+	if page < 0 {
+		http.Error(w, "page is less than zero", http.StatusBadRequest)
+		return
+	}
+	if pageSize <= 0 {
+		http.Error(w, "page_size has to be > 0", http.StatusBadRequest)
+		return
+	}
+
+	db := config.Load().DBConn()
+	packs, err := db.PublicStickerpacks(page, pageSize)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	total, err := db.PublicPacksCount()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(GetPublicPacksResponse{
+		Packs: packs,
+		Total: total,
+	})
+}
