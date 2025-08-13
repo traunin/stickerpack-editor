7c5a1e446963f0618060e68df2300fc47dee2815
Traunin <traunin5@gmail.com>
2025-08-14 02:57:12 +0300

    feat: store name and thumbnail

--- db/init.sql
+++ db/init.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS stickerpacks (
     id SERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL,
     title TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    thumbnail_id TEXT NOT NULL,
     is_public BOOLEAN NOT NULL
 )
