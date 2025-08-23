5c90438fd7c7aa7de6b9d93637866d760749752f
Traunin <traunin5@gmail.com>
2025-08-23 13:14:43 +0300

    feat: dynamic imports

--- apps/website/src/routes.ts
+++ apps/website/src/routes.ts
@@ -1,12 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import StickerpackOverview from '@/views/stickerpack-overview.vue'
-import StickerpackCreate from './views/stickerpack-create.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: '/', component: StickerpackOverview },
-    { path: '/create', component: StickerpackCreate },
+    { 
+      path: '/',
+      component: () => import('@/views/stickerpack-overview.vue')
+    },
+    {
+      path: '/create',
+      component: () => import('@/views/stickerpack-create.vue')
+    },
   ],
 })
 
