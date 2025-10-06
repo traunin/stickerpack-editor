378ddfdeb6cfc82dde222c174ee959116a0405ae
Traunin <traunin5@gmail.com>
2025-10-06 15:22:23 +0300

    feat: svg loading

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import process from 'process'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
+      svgLoader()
     ],
 
     server: {
