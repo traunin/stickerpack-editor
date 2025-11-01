5bb731a843eb153e0c11425a08060dd61a715253
Traunin <traunin5@gmail.com>
2025-11-01 09:12:16 +0300

    feat: graceful shutdown

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Traunin/stickerpack-editor/apps/api/internal/api"
 	"github.com/Traunin/stickerpack-editor/apps/api/internal/config"
@@ -13,19 +19,41 @@ func init() {
 	_, err := exec.LookPath("ffmpeg")
 
 	if err != nil {
-		log.Fatal("no ffmpeg on PATH: %w", err)
+		log.Fatalf("no ffmpeg on PATH: %v", err)
 	}
 }
 
 func main() {
 	cfg := config.Load()
 	handler := api.SetupHandler()
-
 	addr := ":" + cfg.Port()
-	log.Printf("listening on port %s", addr)
-	if err := http.ListenAndServe(addr, handler); err != nil {
-		log.Fatal(err)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: handler,
 	}
 
-	log.Println("stopping...")
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+
+		log.Println("stopped server")
+	}()
+	log.Printf("listening on port %s", addr)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(
+		context.Background(),
+		10*time.Second,
+	)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	log.Println("shutdown complete")
 }
