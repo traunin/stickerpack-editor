d1c574cc112173f2d41d3453557179f26beb09de
Traunin <traunin5@gmail.com>
2026-05-15 00:06:13 +0300

    fix: class import

--- apps/website/src/api/emoji.ts
+++ apps/website/src/api/emoji.ts
@@ -1,9 +1,12 @@
-import Graphemer from 'graphemer'
+import GraphemerImport from 'graphemer'
 
+interface GraphemerModule { default: typeof GraphemerImport }
+const Graphemer =
+  (GraphemerImport as unknown as GraphemerModule).default ?? GraphemerImport
 const emojiRegex = /[\p{Extended_Pictographic}\p{Emoji_Component}]/u
 const splitter = new Graphemer()
 
 export function splitEmojis(text: string): string[] {
   return splitter.splitGraphemes(text)
-    .filter(s => emojiRegex.test(s))
+    .filter((s: string) => emojiRegex.test(s))
 }
